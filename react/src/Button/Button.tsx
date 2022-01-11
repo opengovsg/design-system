c3930b2417c73cd1b9bce3676074936a91d9949d
Kar Rui Lau <karrui.lau@gmail.com>
2022-01-11 15:01:22 +0800

    feat: add initial Button component and foundation design tokens

---
+++ react/src/Button/Button.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps,
+  forwardRef,
+  IconProps,
+} from '@chakra-ui/react'
+
+import { ThemeButtonVariant } from '~theme/components/Button'
+import { ThemeColorScheme } from '~theme/foundations/colours'
+
+import Spinner from '~/Spinner'
+
+export interface ButtonProps extends ChakraButtonProps {
+  /**
+   * The variant of the button.
+   */
+  variant?: ThemeButtonVariant
+
+  /**
+   * Loading spinner font size. Defaults to `1.5rem`.
+   */
+  spinnerFontSize?: IconProps['fontSize']
+
+  /**
+   * Color scheme of button.
+   */
+  colorScheme?: ThemeColorScheme
+}
+
+export const Button = forwardRef<ButtonProps, 'button'>(
+  (
+    {
+      children,
+      spinnerFontSize,
+      isFullWidth,
+      textStyle = 'subhead-1',
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <ChakraButton
+        ref={ref}
+        spinner={<Spinner fontSize={spinnerFontSize ?? '1.5rem'} />}
+        isFullWidth={isFullWidth}
+        {...props}
+        // 15px due to 1px border
+        {...(isFullWidth ? { p: '15px', h: 'auto' } : {})}
+      >
+        <Box textStyle={textStyle}>{children}</Box>
+      </ChakraButton>
+    )
+  },
+)
