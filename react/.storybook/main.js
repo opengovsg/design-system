4c3e40fb7a60ef18621438289bb985fca573146e
Kar Rui Lau <karrui.lau@gmail.com>
2022-01-12 17:11:07 +0800

    feat(storybook): enable storyStoreV7 feature flag

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   features: {
     emotionAlias: false,
+    storyStoreV7: true,
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
