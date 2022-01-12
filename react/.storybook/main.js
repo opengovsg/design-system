8044be15e81b556656207331b7c9b2ec6f8325eb
Kar Rui Lau <karrui.lau@gmail.com>
2022-01-12 17:07:36 +0800

    fix: update storybook config to allow for aliases and ChakraUI

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -1,10 +1,26 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  features: {
+    emotionAlias: false,
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        type: 'javascript/auto',
+        test: /\.mjs$/,
+        include: /node_modules/,
+      },
+    ]
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': path.resolve(__dirname, '../src/'),
+      '~theme': path.resolve(__dirname, '../src/theme'),
+    }
+
+    return config
+  },
 }
