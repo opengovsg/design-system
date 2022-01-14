d8079cda84f5d237a792515818e0ef39b017caf8
Kar Rui Lau <karrui.lau@gmail.com>
2022-01-14 15:20:25 +0800

    feat(storybook): add storybook theme

--- react/.storybook/preview.js
+++ react/.storybook/preview.js
@@ -2,9 +2,14 @@ import '../src/fonts/inter.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../src/theme'
+import { StorybookTheme } from './themes'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  docs: {
+    theme: StorybookTheme.docs,
+    inlineStories: true,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
