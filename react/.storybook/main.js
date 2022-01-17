82c5777c8984653ab84bf5ee8ebd56deb2c4233f
JasonChong96 <jasontjakra@gmail.com>
2022-01-17 17:54:45 +0800

    feat: use mdx and tsx

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -10,10 +10,13 @@ module.exports = {
     './introduction/Welcome/Welcome.stories.tsx',
     './introduction/Principles/Principles.stories.tsx',
     './foundations/**/*.stories.@(mdx|js|jsx|ts|tsx)',
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
   staticDirs: ['../storybook-resources'],
   webpackFinal: async (config) => {
     config.module.rules = [
