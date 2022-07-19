1ba0c8ba54bb38b4a23c975de463e85ac023cee0
Kar Rui <karrui.lau@gmail.com>
2022-07-19 17:44:27 +0800

    feat: update Avatar theming again

--- react/src/theme/components/Avatar.ts
+++ react/src/theme/components/Avatar.ts
@@ -1,29 +1,13 @@
-import { ComponentMultiStyleConfig } from '@chakra-ui/react'
+import { avatarAnatomy as parts } from '@chakra-ui/anatomy'
+import { ThemingProps } from '@chakra-ui/react'
+import { PartsStyleFunction, SystemStyleObject } from '@chakra-ui/theme-tools'
 
 import { textStyles } from '../textStyles'
 
-export const Avatar: ComponentMultiStyleConfig = {
-  parts: ['container', 'badge', 'usernameItem', 'usernameIcon'],
-  sizes: {
-    md: {
-      container: {
-        width: '2.5rem',
-        height: '2.5rem',
-        fontSize: textStyles['subhead-2'].fontSize,
-      },
-      label: textStyles['subhead-2'],
-      badge: {
-        // 20% of container width + 1px border left right.
-        w: 'calc(20% + 2px)',
-        h: 'calc(20% + 2px)',
-      },
-    },
-  },
-  baseStyle: {
+const baseStyle: PartsStyleFunction<typeof parts> = ({ colorScheme: c }) => {
+  return {
     container: {
-      bg: 'primary.500',
-      width: '2.5rem',
-      height: '2.5rem',
+      bg: `${c}.500`,
       color: 'white',
       textStyle: 'subhead-2',
     },
@@ -31,11 +15,47 @@ export const Avatar: ComponentMultiStyleConfig = {
       bg: 'danger.500',
       border: '1px solid white',
       transform: 'none',
-      botton: '1px',
-      right: '1px',
     },
+  }
+}
+
+const getBadgePlacement = (size: ThemingProps['size']): SystemStyleObject => {
+  switch (size) {
+    // Update width and height calculation in the future if needed.
+    default:
+      return {
+        botton: '1px',
+        right: '1px',
+        borderWidth: '1px',
+        // 20% of container width + 1px border left right.
+        w: 'calc(20% + 2px)',
+        h: 'calc(20% + 2px)',
+      }
+  }
+}
+
+const sizes = {
+  xs: { badge: getBadgePlacement('xs') },
+  sm: { badge: getBadgePlacement('sm') },
+  md: {
+    container: {
+      width: '2.5rem',
+      height: '2.5rem',
+      fontSize: textStyles['subhead-2'].fontSize,
+    },
+    label: textStyles['subhead-2'],
+    badge: getBadgePlacement('md'),
   },
+  lg: { badge: getBadgePlacement('lg') },
+  xl: { badge: getBadgePlacement('xl') },
+}
+
+export const Avatar = {
+  parts: parts.keys,
+  sizes,
+  baseStyle,
   defaultProps: {
     size: 'md',
+    colorScheme: 'primary',
   },
 }
