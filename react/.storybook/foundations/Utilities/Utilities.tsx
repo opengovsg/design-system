e7e627be3bf12b82a140235edcb72100be6478ba
Kar Rui <karrui.lau@gmail.com>
2022-12-13 15:41:46 +0800

    feat: update Utility stories

--- react/.storybook/foundations/Utilities/Utilities.tsx
+++ react/.storybook/foundations/Utilities/Utilities.tsx
@@ -1,10 +1,19 @@
 import { useCallback } from 'react'
 import {
+  Box,
   Container,
   Flex,
   Heading,
+  SimpleGrid,
   Stack,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
+  Tr,
   useTheme,
 } from '@chakra-ui/react'
 
@@ -22,71 +31,93 @@ export const Utilities = (): JSX.Element => {
 
   return (
     <Container maxW="container.xl">
-      <Heading
-        mb="2rem"
-        fontSize="4rem"
-        letterSpacing="-0.022em"
-        color="brand.secondary.700"
-      >
-        Utilities
-      </Heading>
-      <Heading
-        as="h2"
-        textStyle="display-2"
-        color="brand.primary.500"
-        mb="2.5rem"
-      >
-        Shadows
-      </Heading>
-      <Stack spacing="2.5rem">
-        <Flex
-          flexDir={{ base: 'column', md: 'row' }}
-          align={{ base: 'start', md: 'center' }}
-        >
-          <Text
-            color="brand.secondary.400"
-            textStyle="subhead-3"
-            mr="10rem"
-            whiteSpace="nowrap"
+      <Stack direction="column" spacing="3rem">
+        <Box>
+          <Heading
+            mb="1.5rem"
+            fontSize="4rem"
+            letterSpacing="-0.022em"
+            color="brand.secondary.700"
           >
-            shadow-sm
+            Utilities
+          </Heading>
+          <Text as="h2" textStyle="h2" color="brand.primary.500" mb="2.5rem">
+            Shadows
           </Text>
-          <Flex h="3.5rem" w="100%" shadow="sm" align="center" justify="center">
-            {prettyPrint('sm', theme.shadows['sm'])}
-          </Flex>
-        </Flex>
-        <Flex
-          flexDir={{ base: 'column', md: 'row' }}
-          align={{ base: 'start', md: 'center' }}
-        >
-          <Text
-            color="brand.secondary.400"
-            textStyle="subhead-3"
-            mr="10rem"
-            whiteSpace="nowrap"
+          <Box></Box>
+          <SimpleGrid
+            columns={[1, 2]}
+            templateColumns={['auto', 'auto 1fr']}
+            spacingX={['0.5rem', '2.5rem']}
+            spacingY="1rem"
           >
-            shadow-md
+            <Text
+              color="brand.secondary.400"
+              textStyle="subhead-3"
+              mr="10rem"
+              alignSelf="center"
+              whiteSpace="nowrap"
+            >
+              shadow-small
+            </Text>
+            <Flex
+              h="3.5rem"
+              w="100%"
+              shadow="small"
+              align="center"
+              justify="center"
+            >
+              {prettyPrint('medium', theme.shadows['small'])}
+            </Flex>
+            <Text
+              color="brand.secondary.400"
+              textStyle="subhead-3"
+              alignSelf="center"
+              mr="10rem"
+              whiteSpace="nowrap"
+            >
+              shadow-medium
+            </Text>
+            <Flex
+              h="3.5rem"
+              w="100%"
+              shadow="medium"
+              align="center"
+              justify="center"
+            >
+              {prettyPrint('medium', theme.shadows['medium'])}
+            </Flex>
+          </SimpleGrid>
+        </Box>
+        <Box>
+          <Text as="h2" textStyle="h2" color="brand.primary.500" mb="1.5rem">
+            Spacing
           </Text>
-          <Flex h="3.5rem" w="100%" shadow="md" align="center" justify="center">
-            {prettyPrint('md', theme.shadows['md'])}
-          </Flex>
-        </Flex>
-        <Flex
-          flexDir={{ base: 'column', md: 'row' }}
-          align={{ base: 'start', md: 'center' }}
-        >
-          <Text
-            color="brand.secondary.400"
-            textStyle="subhead-3"
-            mr="10rem"
-            whiteSpace="nowrap"
-          >
-            shadow-lg
-          </Text>
-          <Flex h="3.5rem" w="100%" shadow="lg" align="center" justify="center">
-            {prettyPrint('lg', theme.shadows['lg'])}
-          </Flex>
-        </Flex>
+          <Table variant="simple">
+            <TableCaption>Spacing</TableCaption>
+            <Thead>
+              <Tr>
+                <Th w="15rem">Variable</Th>
+                <Th w="5rem">Spacing</Th>
+                <Th>Visual</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {Object.entries(theme.space).map(([key, value]) => {
+                const spacing = String(value)
+                return (
+                  <Tr key={key}>
+                    <Td>spacing.{key}</Td>
+                    <Td>{spacing}</Td>
+                    <Td>
+                      <Box bg="blue.50" width={spacing} height={spacing} />
+                    </Td>
+                  </Tr>
+                )
+              })}
+            </Tbody>
+          </Table>
+        </Box>
       </Stack>
     </Container>
   )
