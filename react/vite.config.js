d1a1ff14b76c6036c3f249fae5e8781c23dcfeb1
Kar Rui <karrui.lau@gmail.com>
2022-12-20 19:57:49 +0800

    chore: update build pipeline to be more consistent

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,65 +1,13 @@
-/* eslint-disable no-undef */
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import path from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
-import PackageJson from './package.json'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    dts({
-      outputDir: 'build',
-    }),
-  ],
-  build: {
-    outDir: 'build',
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: '@opengovsg/design-system-react',
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '~': path.resolve(__dirname, './src'),
     },
-    rollupOptions: {
-      external: [
-        'react/jsx-runtime',
-        'react/jsx-dev-runtime',
-        'lodash.mergewith',
-        'lodash',
-        'date-fns-tz',
-        ...Object.keys(PackageJson.peerDependencies),
-      ],
-      output: [
-        {
-          format: 'umd',
-          dir: 'build',
-          entryFileNames: 'index.[format].js',
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            ...PackageJson.peerDependencies,
-          },
-        },
-        {
-          format: 'es',
-          esModule: true,
-          preserveModules: true,
-          preserveModulesRoot: 'src',
-          exports: 'named',
-          dir: 'build',
-          entryFileNames: '[name].js',
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            ...PackageJson.peerDependencies,
-          },
-        },
-      ],
-    },
-    target: 'es6',
-    sourcemap: false,
-    minify: true,
   },
+  plugins: [react()],
 })
