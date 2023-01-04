e27ce8acefb7f177bda3a89d48627d635a37da8c
Kar Rui <karrui.lau@gmail.com>
2023-01-04 15:54:01 +0800

    lint: fix

--- react/.storybook/colorThemes/AskGov/shadows.ts
+++ react/.storybook/colorThemes/AskGov/shadows.ts
@@ -3,7 +3,7 @@
  * Generated on Wed, 04 Jan 2023 07:42:32 GMT
  */
 
-export const shadows =  {
-  sm: "0px 0px 10px 0px rgba(191, 191, 191, 0.5)",
-  md: "0px 0px 20px 0px rgba(104, 104, 104, 0.3)",
-};
+export const shadows = {
+  sm: '0px 0px 10px 0px rgba(191, 191, 191, 0.5)',
+  md: '0px 0px 20px 0px rgba(104, 104, 104, 0.3)',
+}
