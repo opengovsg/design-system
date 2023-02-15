170dfab6be5dcd662acc5c8aac58f3cc562695cd
Kar Rui Lau <karrui@open.gov.sg>
2023-02-15 14:57:47 +0800

    feat: add initial Toolbar and subcomponent declarations

---
+++ react/src/Toolbar/Toolbar.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import { Toolbar, ToolbarProps } from './Toolbar'
+
+export default {
+  title: 'Components/Toolbar',
+  component: Toolbar,
+  decorators: [],
+  tags: ['autodocs'],
+} as Meta<ToolbarProps>
+
+const Template: StoryFn<ToolbarProps> = (args) => <Toolbar {...args} />
+export const Default = Template.bind({})
+Default.args = {}
---
+++ react/src/Toolbar/Toolbar.tsx
@@ -0,0 +1,35 @@
+import { Flex } from '@chakra-ui/react'
+
+import { Button } from '~/Button'
+import { IconButton } from '~/IconButton'
+
+export interface ToolbarProps {}
+
+/**
+ * Container for the toolbar.
+ */
+export const Toolbar = ({}: ToolbarProps): JSX.Element => {
+  return <Flex></Flex>
+}
+
+/**
+ * Component to group togglable buttons to allow selection of only one in a group.
+ */
+export const ToolbarToggleGroup = () => {
+  return <Flex></Flex>
+}
+
+export const ToolbarGroup = () => {
+  return <Flex></Flex>
+}
+
+export const ToolbarToggleButton = () => {
+  return <Flex></Flex>
+}
+
+export const ToolbarDivider = () => {
+  return <Flex></Flex>
+}
+
+export const ToolbarButton = Button
+export const ToolbarIconButton = IconButton
