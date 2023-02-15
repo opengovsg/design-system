eaf72ab2c58e3a5dfc15a2e4cd0fd0594210bbf7
Kar Rui Lau <karrui@open.gov.sg>
2023-02-15 17:52:09 +0800

    feat: reexport Table and subcomponents from package

---
+++ react/src/Table/index.ts
@@ -0,0 +1,14 @@
+import {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+
+export * from './Th'
+
+export { Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Thead, Tr }
