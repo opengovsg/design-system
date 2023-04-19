e45e4ef2b2d1158d83062ce7ada177e5edcede7f
Kar Rui Lau <karrui@open.gov.sg>
2023-04-19 11:38:28 +0800

    feat: add prettier for post-build linting

--- token-gen/themes/BETA dark/shadows.ts
+++ token-gen/themes/BETA dark/shadows.ts
@@ -1,9 +1,9 @@
 /**
  * Do not edit directly
- * Generated on Thu, 06 Apr 2023 06:58:16 GMT
+ * Generated on Wed, 19 Apr 2023 03:37:26 GMT
  */
 
-export const shadows =  {
-  sm: "0px 0px 10px 0px rgba(16, 16, 16, 0.3)",
-  md: "0px 0px 20px 0px rgba(16, 16, 16, 0.5)",
-};
+export const shadows = {
+  sm: '0px 0px 10px 0px rgba(16, 16, 16, 0.3)',
+  md: '0px 0px 20px 0px rgba(16, 16, 16, 0.5)',
+}
