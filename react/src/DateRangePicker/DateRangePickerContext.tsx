8e93530cb194895893fdaef2e4597c15eff74050
Kar Rui Lau <karrui@open.gov.sg>
2023-11-21 12:06:42 +0800

    fix: be explicit with useFormControlProps
    was passing in extra props for some reason >:(

--- react/src/DateRangePicker/DateRangePickerContext.tsx
+++ react/src/DateRangePicker/DateRangePickerContext.tsx
@@ -196,7 +196,9 @@ const useProvideDateRangePicker = ({
     isDisabled: isDisabledProp,
     isReadOnly: isReadOnlyProp,
     isRequired: isRequiredProp,
-    ...props,
+    'aria-describedby': props['aria-describedby'],
+    onFocus: props.onFocus,
+    id: props.id,
   })
 
   const handleInputBlur: FocusEventHandler<HTMLInputElement> = useCallback(
