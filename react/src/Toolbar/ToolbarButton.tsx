ecabd0b92e54958f1b8f6e531dc29647ddfce46e
Kar Rui Lau <karrui@open.gov.sg>
2024-09-18 12:17:21 +0800

    feat: add forwardRef to toolbar buttons

--- react/src/Toolbar/ToolbarButton.tsx
+++ react/src/Toolbar/ToolbarButton.tsx
@@ -1,11 +1,17 @@
+import { forwardRef } from '@chakra-ui/react'
+
 import { Button, ButtonProps } from '~/Button'
 
 import { useToolbarButtonProps } from './utils/useToolbarButtonProps'
 
 export type ToolbarButtonProps = ButtonProps
 
-export const ToolbarButton = (props: ToolbarButtonProps): JSX.Element => {
-  const toolbarButtonProps = useToolbarButtonProps()
+export const ToolbarButton = forwardRef<ToolbarButtonProps, 'button'>(
+  (props, ref): JSX.Element => {
+    const toolbarButtonProps = useToolbarButtonProps()
 
-  return <Button variant="clear" {...toolbarButtonProps} {...props} />
-}
+    return (
+      <Button variant="clear" ref={ref} {...toolbarButtonProps} {...props} />
+    )
+  },
+)
--- react/src/Toolbar/ToolbarIconButton.tsx
+++ react/src/Toolbar/ToolbarIconButton.tsx
@@ -1,13 +1,22 @@
+import { forwardRef } from '@chakra-ui/react'
+
 import { IconButton, IconButtonProps } from '~/IconButton'
 
 import { useToolbarButtonProps } from './utils/useToolbarButtonProps'
 
 export type ToolbarIconButtonProps = IconButtonProps
 
-export const ToolbarIconButton = (
-  props: ToolbarIconButtonProps,
-): JSX.Element => {
-  const toolbarButtonProps = useToolbarButtonProps()
+export const ToolbarIconButton = forwardRef<ToolbarIconButtonProps, 'button'>(
+  (props, ref): JSX.Element => {
+    const toolbarButtonProps = useToolbarButtonProps()
 
-  return <IconButton variant="clear" {...toolbarButtonProps} {...props} />
-}
+    return (
+      <IconButton
+        ref={ref}
+        variant="clear"
+        {...toolbarButtonProps}
+        {...props}
+      />
+    )
+  },
+)
