7ab7f39434136d67b85b42f5b0f44e04138500a7
Christabel Neo <christabelneo@hotmail.com>
2024-10-08 12:55:34 +0800

    feat: add Armoury theme

---
+++ token-gen/themes/Armoury/shadows.ts
@@ -0,0 +1,9 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 26 Sep 2024 07:58:06 GMT
+ */
+
+export const shadows = {
+  sm: '0px 0px 10px 0px rgba(191, 191, 191, 0.5)',
+  md: '0px 0px 20px 0px rgba(104, 104, 104, 0.3)',
+}
