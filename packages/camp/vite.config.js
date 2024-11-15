22500eab454e5c80ec63f7549b86fb6a04283d52
Kar Rui Lau <karrui@open.gov.sg>
2024-11-15 11:09:31 +0800

    refactor: move current design system package into packages/camp

---
+++ packages/camp/vite.config.js
@@ -0,0 +1,36 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+const isExternal = (id) =>
+  !id.startsWith('.') && !id.startsWith('~') && !path.isAbsolute(id)
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: 'design-system-react',
+    },
+    rollupOptions: {
+      external: isExternal,
+      output: {
+        preserveModules: true,
+      },
+    },
+    sourcemap: true,
+  },
+})
