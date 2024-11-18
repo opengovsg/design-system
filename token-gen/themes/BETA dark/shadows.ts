c75aee86fc278f98dad79b921db560624ba8196e
Kar Rui Lau <karrui@open.gov.sg>
2024-11-18 15:54:13 +0800

    feat: convert repo to a turborepo monorepo (#783)
    * feat: add turbo, remove lerna.json
    
    * refactor: move current design system package into packages/camp
    
    * feat: move token-gen into tooling/camp/token-gen
    
    * feat: update github action workflows
    
    * feat: npm i with workspace
    
    * fix: lint/package errors due to regenerated package-lock
    
    * feat: remove extraneous default working-directory in chromatic ci yml
    
    * fix: setup-node ci settings
    
    * fix: update storybook build process due to package-lock change
    
    * fix: package-lock desync
    
    * feat: add back required vite plugins, update vite
    
    * feat: simplify turbo.json
    
    * fix: add all tooling packages into npm workspace
    
    * feat: remove husky config

--- token-gen/themes/BETA dark/shadows.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Thu, 24 Oct 2024 07:48:17 GMT
- */
-
-export const shadows = {
-  sm: '0px 0px 10px 0px rgba(16, 16, 16, 0.3)',
-  md: '0px 0px 20px 0px rgba(16, 16, 16, 0.5)',
-}
